becca7b9e907a544593dd421292d6fcd335e5e9d
Mohammed Hamza <211220035@nitdelhi.ac.in>
2025-01-13 00:53:32 +0530

    Moving to prod

--- app/page.tsx
+++ app/page.tsx
@@ -21,39 +21,29 @@ export default function Home() {
   }
 
   if (session) {
-    return ( // Add this return statement
-      <>
-        Signed in as {session?.accessToken} <br />
-        <Button onClick={() => redirect('/dashboard')}>Dashboard</Button>
-        <Button onClick={() => signOut()}>Sign out</Button>
-      </>
-    );
+    redirect('/dashboard')
   }
 
   return (
-    // <div className="min-h-screen flex items-center justify-center bg-gradient-to-r from-purple-500 to-pink-500">
-    //   <div className="text-center space-y-6 p-8 bg-white rounded-xl shadow-2xl">
-    //     <div className="flex justify-center">
-    //       <Image src="/logo.png" width={100} height={100} alt="Logo" />
-    //     </div>
-    //     <h1 className="text-4xl font-bold text-gray-900">
-    //       YouTube Playlist Retriever
-    //     </h1>
-    //     <p className="text-gray-600 max-w-md">
-    //       Access and manage your YouTube playlists with ease. Sign in with your
-    //       Google account to get started.
-    //     </p>
-    //     <Button
-    //       onClick={() => signIn("google")}
-    //       className="bg-red-600 hover:bg-red-700"
-    //     >
-    //       Sign in with Google
-    //     </Button>
-    //   </div>
-    // </div>
-    <>
-      Not signed in <br />
-      <Button onClick={() => signIn('google')}>Sign in with Google</Button>
-    </>
+    <div className="min-h-screen flex items-center justify-center bg-gradient-to-r from-purple-500 to-pink-500">
+      <div className="text-center space-y-6 p-8 bg-white rounded-xl shadow-2xl">
+        <div className="flex justify-center">
+          <Image src="/logo.png" width={100} height={100} alt="Logo" />
+        </div>
+        <h1 className="text-4xl font-bold text-gray-900">
+          YouTube Playlist Retriever
+        </h1>
+        <p className="text-gray-600 max-w-md">
+          Access and manage your YouTube playlists with ease. Sign in with your
+          Google account to get started.
+        </p>
+        <Button
+          onClick={() => signIn("google")}
+          className="bg-red-600 hover:bg-red-700"
+        >
+          Sign in with Google
+        </Button>
+      </div>
+    </div>
   );
 }
