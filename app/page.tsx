6a3bf4b453294b8d3bf18923d46bfaaa915e8012
Mohammed Hamza <211220035@nitdelhi.ac.in>
2025-01-12 21:46:05 +0530

    redirect fix

--- app/page.tsx
+++ app/page.tsx
@@ -19,7 +19,8 @@ export default function Home() {
   }
 
   if (session) {
-    redirect("/dashboard");
+    window.location.replace("/dashboard");
+    // redirect("/dashboard");
   }
 
   return (
